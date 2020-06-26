9c252834efac4cdceb5ccba37a9393508dc813da
giacomogroppi <giamg01@gmail.com>
2020-06-26 10:54:28 +0200

    nuovo file:             README.md 	nuovo file:             installazione/pip.sh

---
+++ installazione/pip.sh
@@ -0,0 +1,13 @@
+pip3 install opencv-python
+pip3 install rm
+pip3 install psutil
+pip3 install re
+pip3 install pydub
+pip3 install telepot
+pip3 install googletrans
+pip3 install numpy
+pip3 install ast
+pip3 install json, io
+pip3 install pytesseract
+pip3 install speech_recognition
+pip3 install mysql-connector-python-rf
